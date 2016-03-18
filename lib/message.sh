c6c7d85951a1070bc2d5177a802e9c41e934b225
Luiz Henrique Rapatao <rapatao@rapatao.com>
2016-03-18 10:57:26 -0300

    fix in message script

--- lib/message.sh
+++ lib/message.sh
@@ -2,20 +2,19 @@
 
 source /usr/local/devtools/lib/colors.sh;
 
-
-declare -A COLORSOPTS;
-COLORSOPTS["h"]="${YELLOW}%s${CRESET}";
-COLORSOPTS["i"]="[ ${CYAN}..${CRESET} ] %s";
-COLORSOPTS["n"]="[${BYELLOW}NEWS${CRESET}] %s";
-COLORSOPTS["f"]="[${RED}FAIL${CRESET}] %s";
-COLORSOPTS["w"]="[${YELLOW}WARN${CRESET}] %s";
-COLORSOPTS["p"]=" %s";
-COLORSOPTS["pstart"]="[${GREEN}STARTED${CRESET}] %s";
-COLORSOPTS["pstop"]="[${RED}STOPPED${CRESET}] %s";
-COLORSOPTS["pon"]="[${GREEN}ON${CRESET} ] %s";
-COLORSOPTS["poff"]="[${RED}OFF${CRESET}] %s";
-
 function _message() {
+    declare -A COLORSOPTS;
+    COLORSOPTS["h"]="${YELLOW}%s${CRESET}";
+    COLORSOPTS["i"]="[ ${CYAN}..${CRESET} ] %s";
+    COLORSOPTS["n"]="[${BYELLOW}NEWS${CRESET}] %s";
+    COLORSOPTS["f"]="[${RED}FAIL${CRESET}] %s";
+    COLORSOPTS["w"]="[${YELLOW}WARN${CRESET}] %s";
+    COLORSOPTS["p"]=" %s";
+    COLORSOPTS["pstart"]="[${GREEN}STARTED${CRESET}] %s";
+    COLORSOPTS["pstop"]="[${RED}STOPPED${CRESET}] %s";
+    COLORSOPTS["pon"]="[${GREEN}ON${CRESET} ] %s";
+    COLORSOPTS["poff"]="[${RED}OFF${CRESET}] %s";
+
     local OPTIND;
     local msg="";
     local flg=1;
@@ -27,7 +26,6 @@ function _message() {
             h|i|n|f|w|p) flg=0;;
         esac
         shift $(($OPTIND-1));
-
         [ ${flg} -eq 0 ] && {
             msg+=$( printf "${COLORSOPTS[${opts}${OPTARG}]}" "${@}" );
         }
@@ -38,15 +36,5 @@ function _message() {
     printf "\r\033[2K  ${msg}${lf}";
 }
 
-_message -lh "teste 1";
-_message -li "teste 2";
-_message -ln "teste 3";
-_message "teste sem opcao";
-_message -lf "teste 4";
-_message -lw "teste 5";
-_message -lp "teste 6";
-_message -lp start "teste 7";
-_message -lp stop "teste 8";
-_message -lp on "teste 9";
-_message -lp off "teste 10";
-
+_message "$@";
+unset _message;
